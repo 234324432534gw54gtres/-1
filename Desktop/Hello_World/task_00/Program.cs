d4510503bcbf5cac574e9daa6b6a04a16a3ef140
Nikolay <nik1998293@gmail.com>
2022-07-03 18:42:45 +0300

    Решаем задачу № 2

--- Desktop/Hello_World/task_00/Program.cs
+++ Desktop/Hello_World/task_00/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//Console.Write("ВВедите целое число: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+//int square = num * num;
+//Console.WriteLine($"Квадрат числа {num} = {square} ");
+
+//if (num > 5)
+//{
+//    Console.WriteLine("OK!");
+//}
+//else Console.WriteLine("Ошибка!");
+
+//while (num > 5)
+//{
+
+//}
+
+// int a =25;
+// int b = 5;
+
+// if (a==b*b)
+// {
+//     Console.WriteLine ("25 является квадратом 5");
+// }
+// else 
+// {
+//     Console.WriteLine ("25 не является квадратом 5");
+// }
+
+Console.WriteLine("Введите число и получите день недели: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a == 1)
+{
+    Console.WriteLine("понедельник");
+}
+else if (a == 2)
+{
+    Console.WriteLine("вториник");
+}
+else if (a == 3)
+{
+    Console.WriteLine("среда");
+}
+else if (a == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if (a == 5)
+{
+    Console.WriteLine ("Пятница");
+}
+else if (a == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else if (a == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
